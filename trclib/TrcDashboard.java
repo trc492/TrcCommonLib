e013de311760bd348dade8a3b3b8f6f7cf309e8c
trc492 <trc492@live.com>
2021-11-23 17:13:56 -0800

    Refactored more code from FtcDashboard to TrcDashboard.

--- trclib/TrcDashboard.java
+++ trclib/TrcDashboard.java
@@ -28,6 +28,23 @@ package TrcCommonLib.trclib;
  */
 public abstract class TrcDashboard
 {
+    private static final String moduleName = "TrcDashboard";
+    protected static final boolean debugEnabled = false;
+    private static final boolean tracingEnabled = false;
+    private static final TrcDbgTrace.TraceLevel traceLevel = TrcDbgTrace.TraceLevel.API;
+    private static final TrcDbgTrace.MsgLevel msgLevel = TrcDbgTrace.MsgLevel.INFO;
+    protected TrcDbgTrace dbgTrace = null;
+
+    /**
+     * This method clears all the display lines.
+     */
+    public abstract void clearDisplay();
+
+    /**
+     * This method refresh the display lines on the Driver Station.
+     */
+    public abstract void refreshDisplay();
+
     /**
      * This method displays a formatted message in the specified display line on the Driver Station.
      *
@@ -37,9 +54,64 @@ public abstract class TrcDashboard
      */
     public abstract void displayPrintf(int lineNum, String format, Object... args);
 
-    public static final int MAX_NUM_TEXTLINES = 16;
+    /**
+     * This method returns the value of the named boolean data read from the Telemetry class. If the named data does
+     * not exist, it is created and assigned the given default value. Then it is sent to the Driver Station.
+     *
+     * @param key specifies the name associated with the boolean data.
+     * @param defaultValue specifies the default value if it does not exist.
+     * @return boolean data value.
+     */
+    public abstract boolean getBoolean(String key, boolean defaultValue);
+
+    /**
+     * This method sets the named boolean data with the given value and also sends it to the Driver Station.
+     *
+     * @param key specifies the name associated with the boolean data.
+     * @param value specifies the data value.
+     */
+    public abstract void putBoolean(String key, boolean value);
+
+    /**
+     * This method returns the value of the named double data read from the Telemetry class. If the named data does
+     * not exist, it is created and assigned the given default value. Then it is sent to the Driver Station.
+     *
+     * @param key specifies the name associated with the double data.
+     * @param defaultValue specifies the default value if it does not exist.
+     * @return double data value.
+     */
+    public abstract double getNumber(String key, double defaultValue);
+
+    /**
+     * This method sets the named double data with the given value and also sends it to the Driver Station.
+     *
+     * @param key specifies the name associated with the double data.
+     * @param value specifies the data value.
+     */
+    public abstract void putNumber(String key, double value);
+
+    /**
+     * This method returns the value of the named string data read from the Telemetry class. If the named data does
+     * not exist, it is created and assigned the given default value. Then it is sent to the Driver Station.
+     *
+     * @param key specifies the name associated with the string data.
+     * @param defaultValue specifies the default value if it does not exist.
+     * @return string data value.
+     */
+    public abstract String getString(String key, String defaultValue);
+
+    /**
+     * This method sets the named string data with the given value and also sends it to the Driver Station.
+     *
+     * @param key specifies the name associated with the string data.
+     * @param value specifies the data value.
+     */
+    public abstract void putString(String key, String value);
+
+    protected static final int MAX_NUM_TEXTLINES = 16;
     protected static final String displayKeyFormat = "%02d";
     protected static TrcDashboard instance = null;
+    protected final int numLines;
 
     /**
      * This method allows any class to get an instance of the dashboard so that it can display information on its
@@ -51,5 +123,30 @@ public abstract class TrcDashboard
     {
         return instance;
     }   //getInstance
+
+    /**
+     * Constructor: Create an instance of the object.
+     *
+     * @param numLines specifies the number of display lines.
+     */
+    public TrcDashboard(int numLines)
+    {
+        if (debugEnabled)
+        {
+            dbgTrace = new TrcDbgTrace(moduleName, tracingEnabled, traceLevel, msgLevel);
+        }
+
+        this.numLines = numLines;
+    }   //TrcDashboard
+
+    /**
+     * This method returns the number of text lines on the dashboard display.
+     *
+     * @return number of text lines on the dashboard display.
+     */
+    public int getNumTextLines()
+    {
+        return numLines;
+    }   //getNumTextLines
 
 }   //class TrcDashboard
