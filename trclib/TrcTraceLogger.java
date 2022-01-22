84c04de40b7a05d707be0efe2d72c7c4c6fafd83
trc492 <trc492@live.com>
2022-01-22 02:26:58 -0800

    TrcTraceLogger.setEnabled: added code to print stack trace if opening log file failed.

--- trclib/TrcTraceLogger.java
+++ trclib/TrcTraceLogger.java
@@ -96,6 +96,7 @@ public class TrcTraceLogger
             }
             catch (IOException e)
             {
+                e.printStackTrace();
                 throw new RuntimeException("Failed to open trace log file " + traceLogName);
             }
             loggerThread = new Thread(this::loggerTask, traceLogName);
